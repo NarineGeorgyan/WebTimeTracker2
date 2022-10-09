862f213f86d11b3052347bf67fd80e3d2ff45850
Narine Georgyan <narinegeorgyan88@gmail.com>
2022-10-09 22:55:40 +0400

    change edit part

--- src/redux/reducer/setUseSites.js
+++ src/redux/reducer/setUseSites.js
@@ -10,7 +10,7 @@ const setUseSites = (state = { useSites: [] }, action) => {
         (useSite) => useSite.id === action.payload
       );
       return {
-        useSites: afterEditState
+        useSites: [afterEditState],
       };
     }
     case "DELETE_SITE": {
